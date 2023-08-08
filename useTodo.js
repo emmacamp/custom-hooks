0d36841974c1139939fbc5259f4c10eed6b7578b
ecampusano <ecampusano@dgcp.gob.do>
2023-08-08 13:14:58 -0400

    custom-hooks

---
+++ useTodo.js
@@ -0,0 +1,52 @@
+import { useReducer, useEffect } from "react";
+import { todoActions, todoReducer } from "../08-useReducer/todoReducer";
+
+const initialState = []
+
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos')) || [];
+}
+
+export const useTodo = () => {
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init)
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    }, [todos])
+
+
+    const handleNewTodo = (newTodo) => {
+        // console.log('newTodo: ', newTodo);
+        dispatch({
+            type: todoActions.add,
+            payload: newTodo
+        })
+    }
+
+    const handleDelete = (id) => {
+        // console.log('id: ', id);
+        dispatch({
+            type: todoActions.delete,
+            payload: id
+        })
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: todoActions.toggle,
+            payload: id
+        })
+    }
+
+    let todoCount = todos?.length;
+    let todoPendingCount = todos?.filter(todo => !todo.done).length;
+  
+    return {
+        todos,
+        todoCount,
+        todoPendingCount,
+        handleNewTodo,
+        handleDelete,
+        handleToggleTodo,
+  }
+}
